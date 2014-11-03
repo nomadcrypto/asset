f8b13b36a2bc269411dce8cdaf111aa4746fe65f
Crypto Nomad <nomadcrypto@gmail.com>
2014-11-03 09:54:46 -0800

    added support for more than one type of chart. More charts to follow

--- asset_web/asset_web/urls.py
+++ asset_web/asset_web/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'asset_web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^charts/$', 'charts.views.charts', name='charts'),
-    url(r'^charts/(?P<market>(.*))/(?P<pair>(.*))/(?P<step>(.*))$', 'charts.views.chart', name='chart'),
+    #url(r'^charts/(?P<market>(.*))/(?P<pair>(.*))/(?P<step>(.*))$', 'charts.views.chart', name='chart'),
+    url(r'^charts/(?P<chart_type>(.*))/(?P<market>(.*))/(?P<pair>(.*))/(?P<step>(.*))$', 'charts.views.chart', name='chart'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- asset_web/charts/views.py
+++ asset_web/charts/views.py
@@ -10,7 +10,72 @@ from django.core import serializers
 
 from asset.asset import Asset
 
-def chart(request, market, pair, step):
+steps = {"1min":60, "5min":300, "15min":900, "30min":1800, "1hr":3600, "1d":86400}
+
+def setup_asset(request, market, pair, step):
+    
+
+    asset = Asset(market,pair)
+    prices = asset.getTicks(steps[step])
+    asset.setup(prices)
+
+    display_periods = request.GET.get('display_periods')
+
+    return asset, display_periods
+
+
+def build_response(request, asset, chartdata, asset_data, step, display_periods):
+    if display_periods != None:
+        display_periods = int(display_periods)
+        chartdata.extend(asset_data[len(asset_data)-display_periods:])
+        total_prices = len(asset.priceInfo.prices)
+        priceInfo = asset.getPriceInfo(asset.priceInfo.prices[total_prices-display_periods:])
+    else:
+        priceInfo = asset.priceInfo
+        chartdata.extend(asset_data)
+
+    period = "%sx %s" % (str(len(chartdata) -1), steps[step])
+
+    asset_display_data = {"market":asset.market, "pair": asset.pair, "period": period, 
+        "volatility": np.round(priceInfo.volatility*100,2),
+        "average_lhp_diff": np.round(priceInfo.average_lhp_diff*100,2),
+        }
+
+    return {"chartdata":chartdata, "asset_display_data": asset_display_data}
+
+def detrend(asset):
+    total_prices = len(asset.priceInfo.prices)
+    ma_total = len(asset.priceInfo.stats.ma)
+    dma_total = len(asset.priceInfo.stats.dma)
+    total_offset = total_prices - dma_total
+    ma_offset = ma_total-dma_total
+
+    n = 0
+    chartdata = [["Date", "Price High", "Price Low", "12 MA", "Detrend 12ma", "Detrend 12ma OP"],]
+    asset_data = []
+
+    for price in asset.priceInfo.prices[total_offset:]:
+        man = n + ma_offset
+        dma = asset.priceInfo.stats.ma[man] * (1+asset.priceInfo.stats.dma[n])
+        dma_op = asset.priceInfo.stats.ma[man] * (1+asset.priceInfo.stats.dma_op[n])
+        time_close = price[4].strftime("%b %d %H:%M")
+        res = [time_close,price[3],price[2],asset.priceInfo.stats.ma[man], dma, dma_op]
+        asset_data.append(res)
+        n += 1
+    return chartdata, asset_data
+
+
+def chart(request, chart_type, market, pair, step):
+    chart_types = {"detrend": detrend}
+    asset, display_periods = setup_asset(request, market, pair, step)
+    chartdata, asset_data = chart_types[chart_type](asset)
+    response = build_response(request, asset, chartdata, asset_data, step, display_periods)
+    return HttpResponse(simplejson.dumps(response), content_type='application/json')
+
+
+
+
+def chart_old(request, market, pair, step):
     steps = {"1min":60, "5min":300, "15min":900, "30min":1800, "1hr":3600, "1d":86400}
 
     asset = Asset(market,pair)
